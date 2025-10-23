9becdd9b51f55526e77e9ab8453581be3d3b4f4d
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-10-23 10:08:47 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace eserciziFunzioni2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
